6ca5ddccf3724d8dd505392ee727e31e1e1705ff
Nemessiz <54395244+Nemessiz@users.noreply.github.com>
2021-07-20 13:32:07 +0300

    Decent update

--- 5_stavs.php
+++ 5_stavs.php
@@ -4,19 +4,22 @@ include "header.php";                 // (2) Include the header
 ?>
 
 <!-- begin page content -->
-<p><b>Welcome to our web site.</b></p>
-<p style='text-align: center;'>
-We're using PHP to provide you with dynamic content
-for a better web experience.
-</p>
 <div id="container">
 
   <div id="leftThing">
-    Left Side Menu
+    <button class=btn first value=5-001 onclick=myFunction()>5-001</button>
+    <button class=btn first value=5-002 onclick=myFunction()>5-002</button>
+    <button class=btn first value=5-003 onclick=myFunction()>5-003</button>
+    <button class=btn first value=5-004 onclick=myFunction()>5-004</button>
+    <button class=btn first value=5-005 onclick=myFunction()>5-005</button>
+    <button class=btn first value=5-006 onclick=myFunction()>5-006</button>
+    <button class=btn first value=5-007 onclick=myFunction()>5-007</button>
+    <button class=btn first value=5-008 onclick=myFunction()>5-008</button>
+    <button class=btn first value=5-009 onclick=myFunction()>5-009</button>
   </div>
 
   <div id="content">
-    Random Content
+    <img src='/work/img/5_stavs.png' />
   </div>
 </div>
 <!-- end page content -->
--- header.php
+++ header.php
@@ -1,10 +1,47 @@
 <html>
 <head>
-    <link rel="stylesheet" href="css/header.css" />
     <link rel="stylesheet" href="css/main.css" />
     <title><?php echo $title; ?></title>
 </head>
 <body>
+<nav class="menu-container">
+  <!-- burger menu -->
+  <input type="checkbox" aria-label="Toggle menu" />
+  <span></span>
+  <span></span>
+  <span></span>
 
-<h1>Our Web Site</h1>
+  <!-- logo -->
+  <a href="#" class="menu-logo">
+    <img src="https://www.visma.com/contentassets/ce3e4a8f642e4f848ba78168a926d666/logohoy_ny.jpg" alt="My Awesome Website"/>
+  </a>
+
+  <!-- menu items -->
+  <div class="menu">
+    <ul>
+      <li>
+        <a href="#home">
+          5. Stavs
+        </a>
+      </li>
+      <li>
+        <a href="#pricing">
+          6. Stavs
+        </a>
+      </li>
+      <li>
+        <a href="#blog">
+          7. Stavs
+        </a>
+      </li>
+      <li>
+        <a href="#docs">
+          Eddit
+        </a>
+      </li>
+    </ul>
+    <ul>
+    </ul>
+  </div>
+</nav>
 <!-- end header -->
