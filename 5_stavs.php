7c32308934ce7d40085c8f94344f433bbb359fa4
Nemessiz <54395244+Nemessiz@users.noreply.github.com>
2021-07-23 00:31:19 +0300

    Velais update

--- 5_stavs.php
+++ 5_stavs.php
@@ -57,7 +57,52 @@ include "header.php";                 // (2) Include the header
     <div id="tag29" class="main_tag">Featured</div>
     <div id="tag30" class="main_tag">Featured</div>
     <div id="tag31" class="main_tag">Featured</div>
-
+    <!-- Galdu sets  -->
+    <div id="tag32" class="main_tag">Featured</div>
+    <div id="tag33" class="main_tag">Featured</div>
+    <div id="tag34" class="main_tag">Featured</div>
+    <div id="tag35" class="main_tag">Featured</div>
+    <!-- Galdu sets  -->
+    <div id="tag36" class="main_tag">Featured</div>
+    <div id="tag37" class="main_tag">Featured</div>
+    <div id="tag38" class="main_tag">Featured</div>
+    <div id="tag39" class="main_tag">Featured</div>
+    <!-- Galdu sets  -->
+    <div id="tag40" class="main_tag">Featured</div>
+    <div id="tag41" class="main_tag">Featured</div>
+    <div id="tag42" class="main_tag">Featured</div>
+    <div id="tag43" class="main_tag">Featured</div>
+    <!-- Galdu sets  -->
+    <div id="tag44" class="main_tag">Featured</div>
+    <div id="tag45" class="main_tag">Featured</div>
+    <!-- Galdu sets  -->
+    <div id="tag46" class="main_tag">Featured</div>
+    <div id="tag47" class="main_tag">Featured</div>
+    <div id="tag48" class="main_tag">Featured</div>
+    <div id="tag49" class="main_tag">Featured</div>
+    <div id="tag50" class="main_tag">Featured</div>
+    <div id="tag51" class="main_tag">Featured</div>
+    <!-- Galdu sets  -->
+    <div id="tag52" class="main_tag">Featured</div>
+    <div id="tag53" class="main_tag">Featured</div>
+    <div id="tag54" class="main_tag">Featured</div>
+    <div id="tag55" class="main_tag">Featured</div>
+    <div id="tag56" class="main_tag">Featured</div>
+    <div id="tag57" class="main_tag">Featured</div>
+    <!-- Galdu sets  -->
+    <div id="tag58" class="main_tag">Featured</div>
+    <div id="tag59" class="main_tag">Featured</div>
+    <div id="tag60" class="main_tag">Featured</div>
+    <div id="tag61" class="main_tag">Featured</div>
+    <div id="tag62" class="main_tag">Featured</div>
+    <div id="tag63" class="main_tag">Featured</div>
+    <!-- Galdu sets  -->
+    <div id="tag64" class="main_tag">Featured</div>
+    <div id="tag65" class="main_tag">Featured</div>
+    <div id="tag66" class="main_tag">Featured</div>
+    <div id="tag67" class="main_tag">Featured</div>
+    <div id="tag68" class="main_tag">Featured</div>
+    <div id="tag69" class="main_tag">Featured</div>
 
 
 
